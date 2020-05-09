d940be6664724bf0a4856db44afd18bb4d9f1b06
lggy <1170261383@qq.com>
2020-05-09 22:57:50 +0800

    项目初始化

---
+++ src/views/home/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login-container">主页页面</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIndex',
+  components: {},
+  props: {},
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less"></style>

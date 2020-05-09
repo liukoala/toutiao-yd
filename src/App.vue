922b19bd0e55fb904e052f411518b711d59e47b0
lggy <1170261383@qq.com>
2020-05-09 14:31:24 +0800

    字体图标

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,10 @@
     <van-cell-group>
        <van-cell title="单元格" value="内容" />
        <van-cell title="单元格" value="内容" label="描述信息" />
-      </van-cell-group>
+   </van-cell-group>
+   <i class="toutiao toutiao-pinglun"></i>
+  <i class="toutiao toutiao-shanchu"></i>
+
   </div>
 </template>
 
